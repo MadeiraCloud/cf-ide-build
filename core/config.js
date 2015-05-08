d08f37027e1cbda09bbfa80462fbbc1f5747ce1c
MorrisLiang <morris@mc2.io>
2015-05-08 17:16:39 +0800

    0.2.1716 ; DevRepo: MadeiraCloud/h5-ide@48640974fd291f66458b3b8437b881b0a7f391cd

--- core/config.js
+++ core/config.js
@@ -1,3 +1,4 @@
+window.FileVersions={"account/account.js":"3a929238","account/template.js":"9b6c0a6e","core/bundle.js":"19cc3f53","core/config.js":"2f17dfee","core/debug.js":"cfb41559","lib/ace.js":"1e1dbc85","lib/jquery-cookie.js":"35af54d1","lib/require.js":"56cbcc51","lib/vender.js":"7abd1bd1","ui/UI.notification.js":"03d4add8"};
 (function() {
   var getCookie, hosts, location, p, s, scripts, version, _i, _len;
   if (!window) {
