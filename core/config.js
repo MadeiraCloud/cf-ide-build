ca4d237249ceaa6ed54131ef1859d6155cb8a49c
Jimmy Xu <xjimmyshcn@gmail.com>
2015-06-12 19:22:05 +0800

    0.14.1920 ; DevRepo: MadeiraCloud/cloudfielder-ide@21c2b5dd3b9cf4d1d0905c5cf091c7bcd4312a19

--- core/config.js
+++ core/config.js
@@ -1,3 +1,4 @@
+window.FileVersions={"account/account.js":"92428656","api/ApiRequest.js":"0cd2f459","core/bundle.js":"b8492c26","core/config.js":"340e3344","core/debug.js":"3cc11e3a","lib/ace.js":"02b6e3c1","lib/ace/editor-grammar-checker.js":"fed972b8","lib/jquery-cookie.js":"35af54d1","lib/jquery.js":"f389a3e3","lib/require.js":"56cbcc51","lib/vender.js":"53999d10"};
 (function() {
   var getCookie, hosts, location, p, s, scripts, version, _i, _len;
   if (!window) {
